b8c78e6433ddbe184b35d1df1ec74be233d51aea
Alexander Averbeck <aa232442@fh-muenster.de>
2021-12-21 20:06:10 +0100

    day 20 annotations and old version

---
+++ 2021/22/solution.py
@@ -0,0 +1,19 @@
+from time import time
+# Advent of code Year 2021 Day 22 solution
+# Author = Averbea
+# Date = December 2021
+
+start = time()
+
+with open((__file__.rstrip("solution.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+
+
+print("Part One : "+ str(None))
+
+
+
+print("Part Two : "+ str(None))
+
+print("time elapsed: " + str(time() - start))
