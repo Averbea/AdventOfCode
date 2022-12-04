a6f8721ebdce77fa81eabc6744eccc194ec91b65
Averbea <42838519+Averbea@users.noreply.github.com>
2022-12-04 13:50:02 +0100

    Update the template file

--- template.py
+++ template.py
@@ -1,15 +1,48 @@
 import os
 from time import time
 
-start = time()
 
-with open(os.path.dirname(__file__) +"/input.txt", 'r', encoding="UTF-8") as inputFile:
-    inputs = inputFile.read()
+def measure_timing( func):
+    """measures the time needed for executing the given function"""
+    time_start = time()
+    result = func()
+    time_end = time()
+    return time_end-time_start, result
+
+def parse_input():
+    """parses the input file and returns the result"""
+    with open(os.path.dirname(__file__) +"/input.txt", 'r', encoding="UTF-8") as input_file:
+        inputs = input_file.read()
+    # TODO parse input into correct data structure
+    return inputs
 
 
 
-print("Part One : "+ str(None))
 
-print("Part Two : "+ str(None))
+def part_one():
+    """Solution for Part 1"""
+    # data = parse_input()
+    return 0
 
-print("time elapsed: " + str(time() - start))
+def part_two():
+    """Solution for Part 2"""
+    # data = parse_input()
+    return 0
+
+
+
+
+def main():
+    """main method"""
+    time_needed, result = measure_timing(part_one)
+
+    print("Part One : "+ str(result))
+    print("time elapsed: " + str(time_needed))
+
+    time_needed, result = measure_timing(part_two)
+    print("\nPart Two : "+ str(part_two()))
+    print("time elapsed: " + str(time_needed))
+
+
+if __name__ == "__main__":
+    main()
