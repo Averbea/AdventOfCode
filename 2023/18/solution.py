3c49b8cca0fb7fef344790c37172ae003c1a8ef4
Averbea <42838519+Averbea@users.noreply.github.com>
2023-12-18 22:51:53 +0100

    2023 Day 18 part 2 completed

--- 2023/18/solution.py
+++ 2023/18/solution.py
@@ -8,7 +8,7 @@ from utils.templateutils import timeit, read_input_file
 
 def process_input():
     """parses the input file and returns the result"""
-    file = read_input_file(test=True)
+    file = read_input_file(test=False)
     lines = file.splitlines()
     # splitte jede linie in lines (R 11 (#00b1d2)) in 3 teile (string, int, string
     splitted = [[line.split(' ')[0], int(line.split(' ')[1]), line.split(' ')[2][2:-1]] for line in lines]
@@ -16,6 +16,7 @@ def process_input():
 
 
 def fill_trench(trench: set, definetly_inside):
+    """fills trench using flood fill algorithm"""
     to_fill = [definetly_inside]
 
     while to_fill:
@@ -51,25 +52,12 @@ def print_trench(trench):
         print('')
 
 
-def dig_trench(data, use_color=False) -> set[tuple[int, int]]:
+def dig_trench(data) -> set[tuple[int, int]]:
     pos = (0, 0)
     trench = set()
     trench.add(pos)
     for line in data:
         direction, steps, color = line
-
-        if use_color:
-            color_dir = color[-1]
-            steps = int(color, 16)
-            match color_dir:
-                case 0:
-                    direction = 'R'
-                case 1:
-                    direction = 'D'
-                case 2:
-                    direction = 'L'
-                case 3:
-                    direction = 'U'
 
         for i in range(steps):
             match direction:
@@ -91,23 +79,63 @@ def part_one():
     """Solution for Part 1"""
     data = process_input()
     trench = dig_trench(data)
-    # print_trench(trench)
     fill_trench(trench, (2, -8))
-    # print('')
     # print_trench(trench)
     return len(trench)
+
+
+def get_trench_corners(data, use_color=False):
+    circumference = 0
+    pos = (0, 0)
+    trench_corners = [pos]
+    for line in data:
+        direction, steps, color = line
+        if use_color:
+            match color[-1]:
+                case '0':
+                    direction = 'R'
+                case '1':
+                    direction = 'D'
+                case '2':
+                    direction = 'L'
+                case '3':
+                    direction = 'U'
+
+            steps = int(color[:-1], 16)
+
+        match direction:
+            case 'R':
+                pos = (pos[0] + steps, pos[1])
+            case 'L':
+                pos = (pos[0] - steps, pos[1])
+            case 'U':
+                pos = (pos[0], pos[1] + steps)
+            case 'D':
+                pos = (pos[0], pos[1] - steps)
+        trench_corners.append(pos)
+        circumference += steps
+    return trench_corners, circumference
+
+
+def area_inside(corners):
+    """area inside polygon -> shoelace formula"""
+    area = 0
+    for i, corner in enumerate(corners):
+        area += corner[0] * (
+                corners[(i + 1) % len(corners)][1] - corners[(i - 1) % len(corners)][1])
+    area //= 2
+    return abs(area)
 
 
 @timeit
 def part_two():
     """Solution for Part 2"""
     data = process_input()
-    trench = dig_trench(data, True)
-    # print_trench(trench)
-    fill_trench(trench, (2, -8))
-    # print('')
-    # print_trench(trench)
-    return len(trench)
+    trench_corners, circumference = get_trench_corners(data, use_color=True)
+    area = area_inside(trench_corners)
+    # picks theorem: A = i + b/2 - 1     i = points inside, b = boundary points, A = area
+    # A + b = i + b/2 + 1
+    return area + circumference / 2 + 1
 
 
 if __name__ == "__main__":
