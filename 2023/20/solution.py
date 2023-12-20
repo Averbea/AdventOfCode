074efaab449fc8a70ae02dc0b6e81565d6200904
Averbea <42838519+Averbea@users.noreply.github.com>
2023-12-20 22:24:36 +0100

    day 20 part 2 completed

--- 2023/20/solution.py
+++ 2023/20/solution.py
@@ -7,8 +7,6 @@ import re
 
 from utils.templateutils import timeit, read_input_file
 from pyvis.network import Network
-import matplotlib.pyplot as plt
-import networkx as nx
 
 
 def process_input():
@@ -41,7 +39,6 @@ def process_input():
 def process_signal(modules, signal):
     new_signals = []
     src, val, mod = signal
-
     if mod not in modules.keys():
         return new_signals
     module = modules[mod]
@@ -50,20 +47,14 @@ def process_signal(modules, signal):
         case "%":
             if val == 0:
                 module["on"] = not module["on"]
-                if module["on"]:
-                    for target in targets:
-                        new_signals.append((mod, 1, target))
-                else:
-                    for target in targets:
-                        new_signals.append((mod, 0, target))
+                outgoing = 1 if module["on"] else 0
+                for target in targets:
+                    new_signals.append((mod, outgoing, target))
         case "&":
             module["last_pulse"][src] = val
-            if all(module["last_pulse"].values()):
-                for target in targets:
-                    new_signals.append((mod, 0, target))
-            else:
-                for target in targets:
-                    new_signals.append((mod, 1, target))
+            outgoing = 0 if all(module["last_pulse"].values()) else 1
+            for target in targets:
+                new_signals.append((mod, outgoing, target))
         case None:
             if mod == "broadcaster":
                 for target in targets:
@@ -90,11 +81,44 @@ def part_one():
     return amount_low * amount_high
 
 
+def create_graph_vis(modules):
+    nodes = [name for name in modules.keys()]
+    node_titles = [f"{name} {module['type']}" for name, module in modules.items()]
+    nodes.append('rx')
+    node_titles.append('rx')
+    edges = []
+    for name, module in modules.items():
+        edges += [(name, target) for target in module["targets"]]
+    net = Network(directed=True)
+    net.add_nodes(nodes, label=node_titles)
+    net.add_edges(edges)
+    net.show("graph.html", notebook=False)
+
+
 @timeit
 def part_two():
     """Solution for Part 2"""
     modules = process_input()
-    return 0
+    # create_graph_vis(modules)
+    presses = 0
+
+    seen = {}
+    (feed,) = [name for name, module in modules.items() if "rx" in module["targets"]]
+    while len(seen) < 4:
+        presses += 1
+        signals = [('btn', 0, 'broadcaster')]
+
+        while signals:
+
+            signal = signals.pop(0)
+            origin, val, target = signal
+            if target == feed and val == 1:
+                if origin not in seen.keys():
+                    seen[origin] = presses
+            new_signals = process_signal(modules, signal)
+            signals.extend(new_signals)
+
+    return math.lcm(*seen.values())
 
 
 if __name__ == "__main__":
