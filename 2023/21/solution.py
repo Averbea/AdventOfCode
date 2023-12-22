cb6bb99e7c9e8c927cd2b45cd4c30da958df2d14
Averbea <42838519+Averbea@users.noreply.github.com>
2023-12-22 22:09:52 +0100

    day 22 completed

--- 2023/21/solution.py
+++ 2023/21/solution.py
@@ -7,13 +7,15 @@ from utils.templateutils import timeit, read_input_file
 from tqdm import tqdm
 
 
+
+
 def process_input():
     """parses the input file and returns the result"""
-    file = read_input_file(test=False).splitlines()
+    file = read_input_file(test=True).splitlines()
 
-    starting_pos = [(idx, line.index('S'))
-                    for idx, line in enumerate(file) if 'S' in line][0]
+    starting_pos = [(idx, line.index('S')) for idx, line in enumerate(file) if 'S' in line][0]
     return file, starting_pos
+
 
 
 @timeit
@@ -24,7 +26,7 @@ def part_one():
 
     reached_in_steps = {s: set() for s in range(steps+1)}
     reached_in_steps[0].add(starting_pos)
-    for step in range(1, steps+1):
+    for step in range(1,steps+1):
         for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             for pos in reached_in_steps[step-1]:
                 new_pos = (pos[0] + direction[0], pos[1] + direction[1])
@@ -39,8 +41,21 @@ def part_one():
 @timeit
 def part_two():
     """Solution for Part 2"""
+    grid, starting_pos = process_input()
+    steps = 1000
 
-    return 0
+    reached_in_steps = {s: set() for s in range(steps+1)}
+    reached_in_steps[0].add(starting_pos)
+    for step in tqdm(range(1, steps+1)):
+        for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            for pos in reached_in_steps[step-1]:
+                new_pos = (pos[0] + direction[0], pos[1] + direction[1])
+                if grid[new_pos[0] % len(grid)][new_pos[1]%len(grid[0])] == '#':
+                    continue
+                reached_in_steps[step].add(new_pos)
+
+
+    return len(reached_in_steps[steps])
 
 
 if __name__ == "__main__":
---
+++ 2023/22/solution.py
@@ -0,0 +1,139 @@
+""" Advent of code Year 2023 Day 22 solution
+Author = Averbea
+Date = December 2023
+"""
+import copy
+import re
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from utils.templateutils import timeit, read_input_file
+
+
+def process_input():
+    """parses the input file and returns the result"""
+    file = read_input_file(test=False)
+    return [list(map(int, re.findall(r"\d+", line))) for line in file.splitlines()]
+
+
+def plot_as_voxel(cubes_data):
+    dim_x = max([int(x[3]) for x in cubes_data]) + 1
+    dim_y = max([int(x[4]) for x in cubes_data]) + 1
+    dim_z = max([int(x[5]) for x in cubes_data]) + 1
+    # Prepare some coordinates
+    x, y, z = np.indices((dim_x, dim_y, dim_z))
+
+    fig = plt.figure(figsize=(12, 4))  # Adjust the figsize as needed
+    ax1 = fig.add_subplot(131, projection="3d")  # Front view
+    ax2 = fig.add_subplot(132, projection="3d", proj_type='ortho')  # Left view
+    ax3 = fig.add_subplot(133, projection="3d", proj_type='ortho')  # Right view
+
+    for ax, elev, azim in zip([ax1, ax2, ax3], [None, 0, 0], [None, 0, -90]):
+        cubes = False
+        # Draw cuboids
+        for min_x, min_y, min_z, max_x, max_y, max_z in cubes_data:
+            cubes |= (x >= min_x) & (x <= max_x) & \
+                     (y >= min_y) & (y <= max_y) & \
+                     (z >= min_z) & (z <= max_z)
+
+        ax.voxels(cubes, edgecolor='k', facecolors='blue')
+        ax.view_init(elev=elev, azim=azim)  # Set view angle
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+    plt.show()
+
+
+def let_bricks_fall(bricks):
+    amount_fallen = 0
+    bricks.sort(key=lambda x: min(x[2], x[5]))
+    x_length = max([x[3] for x in bricks])
+    y_length = max([x[4] for x in bricks])
+    # plot_as_voxel(bricks)
+    heightmap = np.zeros((x_length + 1, y_length + 1))
+    for brick in bricks:
+        min_x, min_y, min_z, max_x, max_y, max_z = brick
+        assert (min_y <= max_y)
+        assert (min_x <= max_x)
+        assert (min_z <= max_z)
+        max_below = np.max(heightmap[min_y:max_y + 1, min_x:max_x + 1])
+        diff = brick[2] - max_below - 1
+        if diff >= 1:
+            amount_fallen += 1
+
+        brick[2] -= diff
+        brick[5] -= diff
+        heightmap[min_y:max_y + 1, min_x:max_x + 1] = brick[5]
+    return amount_fallen
+
+
+def get_supports_and_supported_by(bricks):
+
+    supports = {}
+    supported_by = {}
+    for idx1, b1 in enumerate(bricks):
+        b1_m_x, b1_m_y, b1_m_z, b1_M_x, b1_M_y, b1_M_z = b1
+
+        supports[idx1] = {}
+
+        # check which bricks are supported by b1
+        for idx2, b2 in enumerate(bricks):
+            support_points = []
+            if idx1 == idx2:
+                continue
+            b2_m_x, b2_m_y, b2_m_z, b2_M_x, b2_M_y, b2_M_z = b2
+            if b2_m_z - 1 != b1_M_z:
+                continue
+            for y in range(b2_m_y, b2_M_y + 1):
+                for x in range(b2_m_x, b2_M_x + 1):
+                    if b1_m_y <= y <= b1_M_y and b1_m_x <= x <= b1_M_x:
+                        support_points.append((x, y))
+            if len(support_points) > 0:
+                supports[idx1][idx2] = support_points
+                if idx2 not in supported_by:
+                    supported_by[idx2] = {}
+                supported_by[idx2][idx1] = support_points
+    return supports, supported_by
+
+@timeit
+def part_one():
+    """Solution for Part 1"""
+    bricks = process_input()
+    # let bricks fall
+    let_bricks_fall(bricks)
+    supports, supported_by = get_supports_and_supported_by(bricks)
+    #plot_as_voxel(bricks)
+    amount_to_safely_remove = 0
+    for idx, brick in enumerate(bricks):
+        bricks_supported_by_this = list(supports[idx].keys())
+        if len(bricks_supported_by_this) == 0:
+            amount_to_safely_remove += 1
+            continue
+        for brick_supported in bricks_supported_by_this:
+            if len(supported_by[brick_supported]) == 1:
+                break
+        else:
+            amount_to_safely_remove += 1
+
+    return amount_to_safely_remove
+
+
+@timeit
+def part_two():
+    """Solution for Part 2"""
+    bricks = process_input()
+    let_bricks_fall(bricks)
+    sum_fallen = 0
+    for idx, brick in enumerate(tqdm(bricks)):
+        new_bricks = copy.deepcopy(bricks)
+        new_bricks.pop(idx)
+        sum_fallen += let_bricks_fall(new_bricks)
+    return sum_fallen
+
+
+if __name__ == "__main__":
+    print("Part One : " + str(part_one()) + "\n")
+    print("Part Two : " + str(part_two()))
